ab1d94c3d928262b2bda3fabada1fdd9010f460f
Michal Kochel <gavoja@gmail.com>
2025-12-14 14:24:09 +0100

    Fixed ESLint config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,10 @@
 import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
-export default neostandard({ ignores: resolveIgnoresFromGitignore() })
+
+export default [
+  ...neostandard({ ignores: resolveIgnoresFromGitignore() }),
+  {
+    rules: {
+      '@stylistic/quote-props': 'off' // Consistency is better.
+    }
+  }
+]
