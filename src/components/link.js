c863eed021a82665b8850bf064cd4db3e20c74fd
Michal Kochel <gavoja@gmail.com>
2025-12-14 14:24:24 +0100

    Fixed tabindex typo.

--- src/components/link.js
+++ src/components/link.js
@@ -3,7 +3,7 @@ import h from '../shared/h.js'
 export function Link ({ href, onMouseDown, children }) {
   return h('a', {
     href,
-    tabindex: '0', // In case href is not set.
+    tabIndex: '0', // In case href is not set.
     onMouseDown,
     tw: 'text-blue-500 cursor-pointer hover:underline inline decoration-1 underline-offset-4 focus:outline-offset-2 px-px',
     children
